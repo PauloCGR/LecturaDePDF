aac82d983f1d1889fb1648486309241890d43fd7
PauloCGR <pauloruelas2003@gmail.com>
2025-01-30 21:43:50 -0600

    Initial commit

---
+++ LecturaDePDF/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿
+namespace LecturaDePDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombreComercial = new System.Windows.Forms.TextBox();
+            this.txtFechaCambioEstado = new System.Windows.Forms.TextBox();
+            this.txtEstatus = new System.Windows.Forms.TextBox();
+            this.txtFecha = new System.Windows.Forms.TextBox();
+            this.txtSApellido = new System.Windows.Forms.TextBox();
+            this.txtPApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCURP = new System.Windows.Forms.TextBox();
+            this.txtRFC = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRegistrar.Location = new System.Drawing.Point(273, 12);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(113, 34);
+            this.btnRegistrar.TabIndex = 0;
+            this.btnRegistrar.Text = "CONSULTAR";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtNombreComercial);
+            this.panel1.Controls.Add(this.txtFechaCambioEstado);
+            this.panel1.Controls.Add(this.txtEstatus);
+            this.panel1.Controls.Add(this.txtFecha);
+            this.panel1.Controls.Add(this.txtSApellido);
+            this.panel1.Controls.Add(this.txtPApellido);
+            this.panel1.Controls.Add(this.txtNombre);
+            this.panel1.Controls.Add(this.txtCURP);
+            this.panel1.Controls.Add(this.txtRFC);
+            this.panel1.Location = new System.Drawing.Point(32, 64);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(606, 275);
+            this.panel1.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(64, 237);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(93, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Nombre Comercial";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(64, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(169, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Fecha de ultimo cambio de estado";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(64, 180);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Estatus en el padron";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(64, 153);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(140, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Fecha inicio de operaciones";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(63, 129);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Segundo Apellido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Primer Apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Nombre (s)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "CURP";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "RFC";
+            // 
+            // txtNombreComercial
+            // 
+            this.txtNombreComercial.Location = new System.Drawing.Point(241, 234);
+            this.txtNombreComercial.Name = "txtNombreComercial";
+            this.txtNombreComercial.ReadOnly = true;
+            this.txtNombreComercial.Size = new System.Drawing.Size(303, 20);
+            this.txtNombreComercial.TabIndex = 8;
+            // 
+            // txtFechaCambioEstado
+            // 
+            this.txtFechaCambioEstado.Location = new System.Drawing.Point(241, 207);
+            this.txtFechaCambioEstado.Name = "txtFechaCambioEstado";
+            this.txtFechaCambioEstado.ReadOnly = true;
+            this.txtFechaCambioEstado.Size = new System.Drawing.Size(303, 20);
+            this.txtFechaCambioEstado.TabIndex = 7;
+            // 
+            // txtEstatus
+            // 
+            this.txtEstatus.Location = new System.Drawing.Point(241, 180);
+            this.txtEstatus.Name = "txtEstatus";
+            this.txtEstatus.ReadOnly = true;
+            this.txtEstatus.Size = new System.Drawing.Size(303, 20);
+            this.txtEstatus.TabIndex = 6;
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Location = new System.Drawing.Point(241, 153);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.ReadOnly = true;
+            this.txtFecha.Size = new System.Drawing.Size(303, 20);
+            this.txtFecha.TabIndex = 5;
+            // 
+            // txtSApellido
+            // 
+            this.txtSApellido.Location = new System.Drawing.Point(241, 126);
+            this.txtSApellido.Name = "txtSApellido";
+            this.txtSApellido.ReadOnly = true;
+            this.txtSApellido.Size = new System.Drawing.Size(303, 20);
+            this.txtSApellido.TabIndex = 4;
+            // 
+            // txtPApellido
+            // 
+            this.txtPApellido.Location = new System.Drawing.Point(241, 99);
+            this.txtPApellido.Name = "txtPApellido";
+            this.txtPApellido.ReadOnly = true;
+            this.txtPApellido.Size = new System.Drawing.Size(303, 20);
+            this.txtPApellido.TabIndex = 3;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(241, 72);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.ReadOnly = true;
+            this.txtNombre.Size = new System.Drawing.Size(303, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // txtCURP
+            // 
+            this.txtCURP.Location = new System.Drawing.Point(241, 45);
+            this.txtCURP.Name = "txtCURP";
+            this.txtCURP.ReadOnly = true;
+            this.txtCURP.Size = new System.Drawing.Size(303, 20);
+            this.txtCURP.TabIndex = 1;
+            // 
+            // txtRFC
+            // 
+            this.txtRFC.Location = new System.Drawing.Point(241, 18);
+            this.txtRFC.Name = "txtRFC";
+            this.txtRFC.ReadOnly = true;
+            this.txtRFC.Size = new System.Drawing.Size(303, 20);
+            this.txtRFC.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(671, 347);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnRegistrar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CONSULTAR PDF";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombreComercial;
+        private System.Windows.Forms.TextBox txtFechaCambioEstado;
+        private System.Windows.Forms.TextBox txtEstatus;
+        private System.Windows.Forms.TextBox txtFecha;
+        private System.Windows.Forms.TextBox txtSApellido;
+        private System.Windows.Forms.TextBox txtPApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCURP;
+        private System.Windows.Forms.TextBox txtRFC;
+    }
+}
+
---
+++ LecturaDePDF/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System.Text.RegularExpressions;
+
+namespace LecturaDePDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ExtraerDatosPDF(string rutaPDF)
+        {
+          
+            using (PdfReader reader = new PdfReader(rutaPDF))
+            {
+
+                for (int pageNumber = 1; pageNumber <= reader.NumberOfPages; pageNumber++)
+                {
+                   
+                    string text = PdfTextExtractor.GetTextFromPage(reader, pageNumber);
+
+                    string rfc = FiltrarInformacion(text, "RFC: ([A-Z0-9]+)");
+                    string curp = FiltrarInformacion(text, "CURP: ([A-Z0-9]+)");
+                    string nombre = FiltrarInformacion(text, "Nombre \\(s\\): ([A-Z]+)");
+                    //string apellido = FiltrarInformacion(text, "Primer Apellido: ([A-ZñÑ]+)");
+                    //string segundoApellido = FiltrarInformacion(text, "Segundo Apellido: ([A-Za-zñÑ ]+)");
+                    string apellido = ObtenerApellidoCompleto(text, "Primer Apellido: ([A-ZñÑ]+)");
+                    var Lista = apellido.Split(' ');
+                    
+                    string fechaInicio = FiltrarInformacion(text, "Fecha inicio de operaciones: ([0-9A-Z ]+)");
+                    string estatus = FiltrarInformacion(text, "Estatus en el padrón: ([A-Za-z ]+)");
+                    string fechaCambio = FiltrarInformacion(text, "Fecha de último cambio de estado: ([0-9A-Z ]+)");
+                    string nombreComercial = FiltrarInformacion(text, "Nombre Comercial: ([A-Za-zñÑ ]+)");
+
+                    txtRFC.Text += rfc + Environment.NewLine;
+                    txtCURP.Text += curp + Environment.NewLine;
+                    txtNombre.Text += nombre + Environment.NewLine;
+                    //txtPApellido.Text += Lista[0] + Environment.NewLine;
+                    //txtSApellido.Text += Lista[1] + Environment.NewLine;
+                    txtPApellido.Text += Lista.Length >= 2 ? Lista[0] : string.Empty + Environment.NewLine;
+                    txtSApellido.Text += Lista.Length >= 2 ? Lista[1] : string.Empty + Environment.NewLine;
+                
+                    txtFecha.Text += fechaInicio + Environment.NewLine;
+                    txtEstatus.Text += estatus + Environment.NewLine;
+                    txtFechaCambioEstado.Text += fechaCambio + Environment.NewLine;
+                    txtNombreComercial.Text += nombreComercial + Environment.NewLine;
+                }
+            }
+        }
+
+        private string FiltrarInformacion(string texto, string patron)
+        {
+            string resultado = string.Empty;
+
+            Match match = Regex.Match(texto, patron);
+            if (match.Success)
+            {
+                resultado = match.Groups[1].Value;
+            }
+
+            return resultado;
+        }
+
+        private string ObtenerApellidoCompleto(string texto, string patron)
+        {
+            string resultado = string.Empty;
+
+            Match match = Regex.Match(texto, patron);
+            if (match.Success)
+            {
+                int indice = match.Index + match.Length; // Obtener el índice después del patrón encontrado
+                string[] lineas = texto.Substring(indice).Split('\n'); // Dividir el texto restante en líneas
+
+                if (lineas.Length >= 2) // Asegurarse de que haya al menos dos líneas
+                {
+                    resultado = match.Groups[1].Value + " " + lineas[1].Trim(); // Concatenar el valor encontrado y el siguiente renglón sin espacios extras
+                }
+            }
+
+            return resultado;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            string rutaPDF = @"C:\Users\Paulo Cesar\Desktop\Lectura\SAT_HOUSE.pdf";
+            ExtraerDatosPDF(rutaPDF);
+
+            MessageBox.Show("Datos extraídos exitosamente");
+        }
+    }
+}
